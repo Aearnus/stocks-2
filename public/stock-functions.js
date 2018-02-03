6a9b80c039bad4218c2ae34d63d920c11cb05237
T L <tslimkemann42@gmail.com>
2018-02-03 11:55:12 -0700

    transaction template

--- public/stock-functions.js
+++ public/stock-functions.js
@@ -5,3 +5,10 @@ function init() {
     i("stockValue").textContent = stock["averageValue"];
     i("valueChange").textContent = stockHistoryToChange(stock);
 }
+
+function createTransactionView(time, shares, shareValue, uuid) {
+    console.log("Creating transaction view for transaction " + uuid);
+    var template = i("transactionTemplate").cloneNode(true).content;
+    template.querySelector(".transactionTime").textContent = Math.floor(Date.now()/1000) - time ;
+    //TODO
+}
